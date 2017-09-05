972560216395ea70ea14a9a4c3870a3486db2727
George Kaimakis <geomatus@mac.com>
2017-09-05 01:32:19 +0100

    Adds race stats and results:
    Extracts the 'x' position tuple of each 'turtle' at end of race and uses
    this to determine which player got furthest, the 'max()' function is used
    for this, this is then followed by an 'if/elif/else' structure to assign
    the max player a 'winner' name which is then used for the final finish
    statement.

--- turtle_01.py
+++ turtle_01.py
@@ -2,28 +2,28 @@ from turtle import Turtle
 from random import randint
 
 laura = Turtle()
-laura.color('red')
+laura.color('red', 'white')
 laura.shape('turtle')
 laura.penup()
 laura.goto(-160, 100)
 laura.pendown()
 
 lauren = Turtle()
-lauren.color('blue')
+lauren.color('blue', 'white')
 lauren.shape('turtle')
 lauren.penup()
 lauren.goto(-160, 70)
 lauren.pendown()
 
 rik = Turtle()
-rik.color('green')
+rik.color('green', 'white')
 rik.shape('turtle')
 rik.penup()
 rik.goto(-160, 40)
 rik.pendown()
 
 carrieanne = Turtle()
-carrieanne.color('pink')
+carrieanne.color('black', 'white')
 carrieanne.shape('turtle')
 carrieanne.penup()
 carrieanne.goto(-160, 10)
@@ -34,3 +34,24 @@ for movement in range(100):
     lauren.forward(randint(1, 5))
     rik.forward(randint(1, 5))
     carrieanne.forward(randint(1, 5))
+
+print('laura: \t\t{}'.format(laura.pos()[0]))
+print('lauren: \t{}'.format(lauren.pos()[0]))
+print('rik: \t\t{}'.format(rik.pos()[0]))
+print('carrieanne: \t{}'.format(carrieanne.pos()[0]))
+print('-' * 25)
+
+win = max(laura.pos()[0], lauren.pos()[0], rik.pos()[0], carrieanne.pos()[0])
+
+if win == laura.pos()[0]:
+    winner = 'laura'
+elif win == lauren.pos()[0]:
+    winner = 'lauren'
+elif win == rik.pos()[0]:
+    winner = 'rik'
+else:
+    winner = 'carrieanne'
+
+
+print('The winner is {}!'.format(winner))
+print('-' * 25)
