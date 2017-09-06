186c6c8070bc31edaa312e21fb55ac0f285ddb5a
George Kaimakis <geomatus@mac.com>
2017-09-06 01:23:13 +0100

    Adds set & get _name, and find & use _item:

--- item_01.py
+++ item_01.py
@@ -8,6 +8,14 @@ class Item():
         self.name = item_name
         self.description = None
 
+    def set_name(self, item_name):
+        """sets the item name"""
+        self.name = item_name
+
+    def get_name(self):
+        """sets the item name"""
+        return self.name
+
     def set_description(self, item_description):
         """sets the Item description"""
         self.description = item_description
@@ -18,22 +26,22 @@ class Item():
 
     def find_item(self):
         """method to find item"""
-        pass
+        print('You found the {}!'.format(self.name))
 
     def use_item(self):
         """method to perform action with Item"""
-        pass
+        print('You are using the {}!'.format(self.name))
 
 
 class Lantern(Item):
     """Lantern class"""
 
-    def __init__(self, item_name):
-        super().__init__(self, item_name)
+    # def __init__(self, item_name):
+    #     super().__init__(self, item_name)
 
 
 class Compass(Item):
     """Compass class"""
 
-    def __init__(self, item_name):
-        super().__init__(self, item_name)
+    # def __init__(self, item_name):
+    #     super().__init__(self, item_name)
